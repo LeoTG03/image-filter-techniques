6cf774a06d5651540c6466d22a97be2960bd1585
LeoTG03 <95186980+LeoTG03@users.noreply.github.com>
2024-03-11 23:52:31 -0600

    4th commit: comments on filtering code

--- test-image-filtering.py
+++ test-image-filtering.py
@@ -5,20 +5,21 @@ from matplotlib import pyplot as plt
 import cvlib
 
 
-def run_pipeline():
+def run_pipeline(): #Funcion principal
 
-    args = cvlib.parse_user_data()
+    args = cvlib.parse_user_data() #Funciones de cvlib para mostrar la imagen
     img = cvlib.load_image(args.input_image)
 
     if img is None:
         print('No hay imagen para cargar')
         return
     
-    blur = cv.blur(img,(5,5))
+    blur = cv.blur(img,(5,5))       #Filtros de OpenCV
     blur2 = cv.GaussianBlur(img,(5,5),0)
     median = cv.medianBlur(img,5)
 
-    cvlib.visualise_image(blur, "Average Filter")
+    cvlib.visualise_image(img,"Imagen original")
+    cvlib.visualise_image(blur, "Average Filter") #Exponer las imagenes con los titulos pertinetes
     cvlib.visualise_image(blur2, "Gaussian Filter")
     cvlib.visualise_image(median, "Median Filter")
 
@@ -26,5 +27,5 @@ def run_pipeline():
 
     print('Program finished! \n')
 
-if __name__ == '__main__':
+if __name__ == '__main__': #ejecutar la funcion principal
     run_pipeline() 
